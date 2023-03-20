87c01a3b7edc86084d6f245841c5c2327834bffa
Elektra-V <vedika.chauhan@outlook.com>
2023-03-20 19:49:42 +0530

    Add Multiclient Server

--- sockets/client.py
+++ sockets/client.py
@@ -1,35 +1,53 @@
-import socket
+import socket 
 
 class Client:
-
     def __init__(self, host, port):
         self.host = host
         self.port = port
-    
-    
-    def __str__(self):
-        return f"Hello I am client"
-
 
     def connect(self):
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect((self.host, self.port))
-            message = input(" -> ")
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket.connect((self.host, self.port))
 
-            while message.lower().strip() != "quit":
-                s.send(message.encode())
-                data = s.recv(1024).decode()
-                print(f"Received from server: {data}")
-                message = input(" -> ")
+        # client is connected to the server 
+        print(f"Connected to server {self.host}:{self.port}")
+        self.start_receiving()
+    
+    def start_receiving(self):
+        while True:
+            message = input("Enter message: ")
+            self.send_message(message)
+
+            try:
+                data = self.client_socket.recv(1024)
+
+                if data:
+                    message = data.decode('utf-8').strip()
+                    print(message)
+                    
+                    # if client inputs quit it should close the connection
+                    if message == "quit":
+                        print("Disconnecting from server")
+                        self.client_socket.close()
+                        return
+                    
+                else:
+                    print(f"Disconnected from server")
+                    self.client_socket.close()
+                    return
             
-            s.close()
+            except:
+                print("Error occured while receiving mesaage from server")
+                self.client_socket.close()
+                return
+    
+    def send_message(self,message):
+        try:
+            self.client_socket.send(message.encode('utf-8'))
+        except:
+            print("Error occured while sending message to server")
 
 
-def main():
-    client = Client(socket.gethostname(), 8080)
-    print(client)
+if __name__ == '__main__':
+    client = Client('localhost', 5000)
     client.connect()
-
-
-if __name__ == "__main__":
-    main()
--- sockets/server.py
+++ sockets/server.py
@@ -1,38 +1,80 @@
 import socket
+import threading
 
-class Server:
-    def __init__(self, port, host=socket.gethostname()):
-        self.port = port
+class MultiClientServer:
+
+    def __init__(self, host, port):
         self.host = host
+        self.port = port
+        self.client_list = []
+        self.client_sockets = []
+        self.lock = threading.Lock()
+
+    def start_server(self):
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server_socket.bind((self.host, self.port))
+        self.server_socket.listen(5)
+        # server's own IP address and Port number
+        print(f"Server listening on {self.host}:{self.port}")
+
+        while True:
+            #server connects to specific client socket having some IP address
+            client_socket, client_address = self.server_socket.accept()
+            print(f"New client connected from {client_address[0]}:{client_address[1]}")
+            # blocking the sockets list for adding new sockets to it by one thread at a time
+            self.lock.acquire()
+            self.client_sockets.append(client_socket)
+            self.lock.release()
+
+            client_thread = threading.Thread(target=self.handle_client, args=(client_socket, client_address))
+            # blocking the clients list for adding new client address to it by one thread at a time avoiding race condition
+            self.lock.acquire()
+            self.client_list.append(client_address[0])
+            self.lock.release()
+
+            client_thread.start()
+
+            print(self.client_list)
+
+    def handle_client(self, client_socket, client_address):
+        while True:
+            try:
+                data = client_socket.recv(1024)
+
+                if data:
+                    message = f"Received message from {client_address[0]}:{client_address[1]}: {data.decode('utf-8').strip()}"
+                    print(message)
+
+                    # iterate over list of sockets from clients and brodcast message to all clients
+                    for socket in self.client_sockets:
+                        if socket != client_socket:
+                            socket.send(message.encode('utf-8'))
+
+                else:
+                    print(f"Client {client_address[0]}:{client_address[1]} disconnected")
+
+                    # blocking the clients list for removing client address from it if user gets disconnected abruptly
+                    self.lock.acquire()
+                    self.client_list.remove(client_address[0])
+                    self.client_sockets.remove(client_socket)
+                    self.lock.release()
+
+                    client_socket.close()
+                    return
+                
+            except:
+                print(f"Error occurred while receiving message from {client_address[0]}:{client_address[1]}")
+
+                # blocking the clients list for removing client address from it if there is error while receiving message
+                self.lock.acquire()
+                self.client_list.remove(client_address[0])
+                self.lock.release()
+
+                client_socket.close()
+                return
 
 
-    def __str__(self):
-        return "Hello I am server"
-    
-    
-    def listen(self):
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.bind((self.host, self.port))
-            s.listen()
-            conn, addr = s.accept()
-            print(f"Connected by {addr}")
-
-            while True:
-                data = conn.recv(1024).decode()
-                if not data:
-                    break
-                print(f"from connected user: {data}")
-                data = input(" -> ")
-                conn.send(data.encode())
-
-            conn.close()
-
-
-def main():
-    server = Server(8080)
-    print(server)
-    server.listen()
-   
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    server = MultiClientServer('localhost', 5000)
+    server.start_server()
